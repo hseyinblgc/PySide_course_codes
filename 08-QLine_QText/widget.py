b657f57dcfb1586e87aa4d4991e6f097ada04a94
hseyinblgc <h.bilgic1818@gmail.com>
2026-01-01 17:37:27 +0000

    QLabel and QLineEdit

--- 08-QLine_QText/widget.py
+++ 08-QLine_QText/widget.py
@@ -1,7 +1,31 @@
-from PySide6.QtCore import QLocale, QPoint, QRect, QSize, Qt
-from PySide6.QtGui import QCursor, QFont, QIcon, QPalette, QRegion
-from PySide6.QtWidgets import QSizePolicy, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("QLabel and QLineEdit")
+
+        #A set of signals we can connect to
+        label = QLabel("Fullname: ")
+        self.line_edit = QLineEdit()
+
+        button = QPushButton("Grab Data")
+        button.clicked.connect(self.print_data)
+        self.text_holder_label = QLabel("I AM HERE")
+
+        h_layout = QHBoxLayout()
+        h_layout.addWidget(label)
+        h_layout.addWidget(self.line_edit)
+
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_layout)
+        v_layout.addWidget(button)
+        v_layout.addWidget(self.text_holder_label)
+
+        self.setLayout(v_layout)
+
+        # Slots
+    def print_data(self):
+        self.text_holder_label.setText(self.line_edit.text())
+        self.line_edit.clear()
