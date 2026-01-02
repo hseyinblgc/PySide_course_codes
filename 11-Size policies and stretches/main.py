6871bc6c588564ae8b172d83e2488d85aad4253f
hseyinblgc <h.bilgic1818@gmail.com>
2026-01-02 16:01:25 +0000

    Size Policies

---
+++ 11-Size policies and stretches/main.py
@@ -0,0 +1,39 @@
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy, QLineEdit, QPushButton
+
+import sys
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Size Policies and stretches")
+        
+        label = QLabel("Some Text : ")
+        line_edit = QLineEdit()
+        line_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        h_layout_1 = QHBoxLayout()
+        h_layout_1.addWidget(label)
+        h_layout_1.addWidget(line_edit)
+
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+
+        h_layout_2 = QHBoxLayout()
+        h_layout_2.addWidget(button_1,2)
+        h_layout_2.addWidget(button_2,1)
+        h_layout_2.addWidget(button_3,1)
+
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_2)
+        self.setLayout(v_layout)
+
+app = QApplication(sys.argv)
+
+widget = Widget()
+widget.show()
+
+app.exec()
